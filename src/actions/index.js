af2d43acc86d57ed14dec1fe3adf033bf6c9afb8
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-16 23:50:27 +0530

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,8 @@
 export function selectBook(book){
-  console.log('A book has been selected:', book.title);
+  // selectBook is an ActionCreator, it needs to return an action,
+  // an oject with an type property
+  return{
+   type: 'BOOK_SELECTED',
+   payload: book;
+  };
 }
