16c38ae46518dac0bdd70d7fff0781823749a825
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-16 23:31:37 +0530

    Update book_list.js

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -8,9 +8,12 @@ class Booklist extends Component{
 	renderList(){
 		return this.props.books.map((book) => {
 			return(
-             <li key = {book.title} className = "list-group-item">{book.title}</li>
-			);
-		});
+             <li 
+             key = {book.title} 
+             onClick={() => this.props.selectBook(book)}
+             className = "list-group-item">
+             {book.title}
+             </li>
     }
 
     render(){
