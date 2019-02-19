225165322362f2f8f19af8047151607ac4afb7c5
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-20 01:58:36 +0530

    Update book_detail.js

--- src/containers/book_detail.js
+++ src/containers/book_detail.js
@@ -3,11 +3,13 @@ import { connect } from 'react-redux';
 
 class BookDetail extends Component{
 	
-	 if(!this.props.book){
-   	  return <div>Select a book to get started.</div>
-   }
+	
 	
 	render(){
+		if (!this.props.book ){
+   	      return <div>Select a book to get started.</div>;
+        }
+		
 		return (
         <div>
         <h3>Details for:</h3>
