1268fec7c94b43005dd94eae4cdc762670f6df67
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-22 01:54:06 +0530

    Update reducer_active_book.js

--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -2,7 +2,7 @@
 // this reducer is responsible for 
 export default function(state = null, action){
 	switch(action.type){
-		case: 'BOOK_SELECETD':
+		case: 'BOOK_SELECTED':
          return action.payload;
 	}
    return state
